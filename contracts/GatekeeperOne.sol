1316214f962c2c233f75e80f55aca1a83909fd3a
maborri <matiasdborri@gmail.com>
2022-07-21 15:53:46 -0300

    cambios de prueba 2

--- contracts/GatekeeperOne.sol
+++ contracts/GatekeeperOne.sol
@@ -21,9 +21,7 @@ contract GatekeeperOne {
   }
 
   modifier gateThree(bytes8 _gateKey) {
-      require(uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)), "GatekeeperOne: invalid gateThree part one");
-      require(uint32(uint64(_gateKey)) != uint64(_gateKey), "GatekeeperOne: invalid gateThree part two");
-      require(uint32(uint64(_gateKey)) == uint16(tx.origin), "GatekeeperOne: invalid gateThree part three");
+     
     _;
   }
 
